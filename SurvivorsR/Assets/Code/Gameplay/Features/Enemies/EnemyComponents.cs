fae93756a4f3c6dc4139cf588b445637879d72d4
Refraktometr1 <filatovsj@gmail.com>
2024-09-03 13:08:44 +0300

    added base structure

---
+++ SurvivorsR/Assets/Code/Gameplay/Features/Enemies/EnemyComponents.cs
@@ -0,0 +1,11 @@
+﻿using Entitas;
+
+namespace Code.Gameplay.Features.Enemies
+{
+    public class EnemyComponents
+    {
+        [Game] public class Enemy : IComponent {  }
+        [Game] public class AggroRadius : IComponent { public float Value; }
+        [Game] public class PursuitRadius : IComponent { public float Value; }
+    }
+}
---
+++ SurvivorsR/Assets/Code/Gameplay/Features/Enemies/Registrars/EnemyRegistrar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Code.Common.Entity;
+using Code.Common.Extensions;
+using Code.Gameplay.Features.Movement;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Gameplay.Features.Enemies.Registrars
+{
+    public class EnemyRegistrar : MonoBehaviour
+    {
+        public float Speed;
+        public float AggroRadius;
+        public float PursuitRadius;
+        
+        private GameEntity _entity;
+
+        private void Awake()
+        {
+            _entity = CreateEntity.Empty()
+                .AddSpeed(Speed)
+                .AddTransform(transform)
+                .AddWorldPosition(transform.position)
+                .AddDirection(Vector2.zero)
+                .AddAggroRadius(AggroRadius)
+                .AddPursuitRadius(PursuitRadius)
+                .With(x => x.isEnemy = true)
+                .With(x => x.isMoving = false);
+        }
+    }
+}
